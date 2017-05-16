e5491f6ec220ae4a198d505df66ccd6e83ca5fb9
Bradley Norris <bradnor@Bradley-Norriss-MacBook-Pro.local>
2017-05-15 20:57:28 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,54 @@
+// ES6 - ECMAScript6
+
+// //Good Naming
+//
+// var lowercase;
+// var camelCase;
+// var use_underscore;
+// var _underscores;
+// var $dollarsign;
+//
+// //Meh
+//
+// var SuperCamelCase;
+//
+// //Bad
+//
+// var no spaces;
+// var no-hyphens;
+// var 1nope;
+
+console.log('Sending a message to the console');
+console.info('Sending an information message');
+console.warn('Heres a warning');
+console.error('Here is an error message');
+console.log('emmet: use log+tab');
+
+// Single Line Comment
+
+/*
+Multi
+Line
+Comments
+*/
+
+var
+                  freedom
+        =
+            "A string of javascript";
+console.log(freedom);
+
+// template string
+
+var javascripttemplatestring=`
+  and
+  type
+  whatever
+  you
+  want
+  :)
+
+
+`;
+
+console.log(javascripttemplatestring);
